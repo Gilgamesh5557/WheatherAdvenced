b183038a0c0d5dfd8ca49bbb89dd99c678bf45f0
Gilgamesh5557 <118034822+Gilgamesh5557@users.noreply.github.com>
2023-03-18 08:42:50 +0500

    df

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9a2ff31b65msh1c3e16be157d8b1p145f62jsn3add4a43c5a1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ca069d5082b11bb13bfc474ba8ae2da0";
+
